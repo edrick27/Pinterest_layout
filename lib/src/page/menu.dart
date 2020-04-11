7522c495206aaeac23fb15c5e482da86d29f810f
edrick27 <edricklpcr@gmail.com>
2020-04-10 23:26:36 -0600

    final menu

---
+++ lib/src/page/menu.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+class PinterestButton {
+
+  final Function onpress;
+  final IconData icon;
+
+  PinterestButton({
+    @required this.onpress,
+    @required this.icon
+  });
+  
+}
+
+class PinterestMenu extends StatelessWidget {
+
+  bool mostrar;
+  final List<PinterestButton> items;
+
+  PinterestMenu({
+    @required this.items,
+    this.mostrar,
+  });
+
+  
+  
+  @override
+  Widget build(BuildContext context) {
+
+    return ChangeNotifierProvider(
+      create: (_) => new _menuModel(),
+      child: AnimatedOpacity(
+        duration: Duration(milliseconds: 250),
+        opacity: mostrar ? 1 : 0,
+        child: _PinterestMenuBackground(
+          child: _MenuItems( menuItems: items )
+        )
+      )
+    );
+  
+  }
+
+}
+
+class _PinterestMenuBackground extends StatelessWidget {
+  
+  final Widget child;
+
+  _PinterestMenuBackground({
+    @required this.child
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+      width: 250,
+      height: 60,
+      padding: EdgeInsets.symmetric(horizontal: 17),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius:  BorderRadius.all( Radius.circular(100) ),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black45,
+            offset: Offset(3, 5),
+            blurRadius: 10,
+            spreadRadius: -5
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class _MenuItems extends StatelessWidget {
+
+   final List<PinterestButton> menuItems;
+
+   _MenuItems({
+     this.menuItems
+   });
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: List.generate(menuItems.length, (i) => _menuButton(i, this.menuItems[i]))
+    );
+  
+  }
+
+}
+
+class  _menuButton extends StatelessWidget {
+
+  int index;
+  PinterestButton button;
+
+  _menuButton(this.index, this.button);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final itemSeleccionado = Provider.of<_menuModel>(context).indexSelecionado;
+    
+    return GestureDetector(
+      onTap: (){
+        Provider.of<_menuModel>(context, listen: false).indexSelecionado = this.index;
+        button.onpress();
+      },
+      behavior: HitTestBehavior.translucent,
+      child: Container(
+        child: Icon(
+          button.icon,
+          size: (itemSeleccionado == this.index) ? 30 : 24,
+          color: (itemSeleccionado == this.index) ? Colors.black : Colors.blueGrey,
+        ),
+      ),
+    );
+  }
+}
+
+class _menuModel with ChangeNotifier {
+  
+  int _indexSelecionado = 0;
+
+  int get indexSelecionado => _indexSelecionado;
+
+  set indexSelecionado(int indexSelecionado) {
+    this._indexSelecionado = indexSelecionado;
+    notifyListeners();
+  }
+
+}
---
+++ lib/src/page/pinterest_page.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:pinterest_layout/src/page/menu.dart';
+import 'package:provider/provider.dart';
+
+class PinterestPage extends StatelessWidget {
+
+  final List<PinterestButton> items = [
+    PinterestButton(icon: Icons.pie_chart, onpress: (){ print('tap pie_chart'); }),
+    PinterestButton(icon: Icons.search, onpress: (){ print('tap search'); }),
+    PinterestButton(icon: Icons.notifications, onpress: (){ print('tap notifications'); }),
+    PinterestButton(icon: Icons.supervised_user_circle, onpress: (){ print('tap supervised_user_circle'); }),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ChangeNotifierProvider(
+      create: (_) => _menuModel(),
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            _CreateGrid(),
+            _PrinterestMenuLocation()
+          ],
+        ),
+
+        // body: createGrid(),
+        // bottomNavigationBar: PinterestMenu()
+      ),
+    );
+
+  }
+}
+
+
+class _CreateGrid extends StatefulWidget {
+
+  _CreateGrid();
+
+  @override
+  __CreateGridState createState() => __CreateGridState();
+}
+
+class __CreateGridState extends State<_CreateGrid> {
+
+  final List items = List.generate(200, (i) => i);
+  ScrollController controller = new ScrollController();
+  double scrollAnterior = 0 ;
+
+
+  @override
+  void initState() {
+
+    controller.addListener(() {
+
+      print("scroll listener ${ controller.offset }");
+
+      Provider.of<_menuModel>(context, listen: false).mostrar = ! ( controller.offset > scrollAnterior && controller.offset > 100);
+
+      scrollAnterior = controller.offset;
+
+    });
+
+    super.initState();
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      child: StaggeredGridView.countBuilder(
+        controller: controller,
+        crossAxisCount: 4,
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index){
+          
+          return Container(
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all( Radius.circular(30) ),
+              color: Colors.blue,
+            ),
+            child: new Center(
+              // child: new CircleAvatar(
+              //   backgroundColor: Colors.white,
+              //   child: new Text('$index')
+              // )
+            )
+          );
+        },
+        staggeredTileBuilder: (int index) => StaggeredTile.count(2, index.isEven ? 2 : 3),
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+      )
+    );
+  }
+}
+
+class _PrinterestMenuLocation extends StatelessWidget {
+
+  const _PrinterestMenuLocation();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final width = MediaQuery.of(context).size.width;
+
+    final mostrar = Provider.of<_menuModel>(context).mostrar;
+
+    return Positioned(
+      bottom: 25,
+      child: Container(
+        width: width,
+        child: Align(
+          child: PinterestMenu(
+            items: [
+              PinterestButton(icon: Icons.pie_chart, onpress: (){ print('tap pie_chart'); }),
+              PinterestButton(icon: Icons.search, onpress: (){ print('tap search'); }),
+              PinterestButton(icon: Icons.notifications, onpress: (){ print('tap notifications'); }),
+              PinterestButton(icon: Icons.supervised_user_circle, onpress: (){ print('tap supervised_user_circle'); }),
+            ],
+            mostrar: mostrar
+          )
+        )
+      )
+    );
+  }
+}
+
+class _menuModel with ChangeNotifier {
+
+  bool _mostrar = true;
+
+  bool get mostrar => _mostrar;
+
+  set mostrar(bool value){
+    _mostrar = value;
+    notifyListeners();
+  }
+  
+}
